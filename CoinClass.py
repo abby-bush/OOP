a3644f2ccf2f60e6c125fb02feb5e5a88a5c442a
Abby <abby_bush1@baylor.edu>
2023-02-08 14:17:04 -0600

    class 02-08

--- CoinClass.py
+++ CoinClass.py
@@ -3,26 +3,35 @@ import random
 # The Coin class simulates a coin that can
 # be flipped.
 
+# class is a reserved word in python
+# use upper case for your object definition
+# init (short for initialization) is always first
+
+
 class Coin:
     # The _ _init_ _ method initializes the
     # sideup data attribute with 'Heads'.
 
     def __init__(self):
-        self.sideup = 'Heads'
+        self.sideup = "Heads"  # attribute
+
+    # mutator method--we want the method to control the attribute
+    # aka set method (as opposed to get methods)
+    # mutator changes value of attribute, accessor just looks at it
 
     # The toss method generates a random number
     # in the range of 0 through 1. If the number
     # is 0, then sideup is set to 'Heads'.
     # Otherwise, sideup is set to 'Tails'.
 
-    def toss(self):
+    def toss(self):  # method
         if random.randint(0, 1) == 0:
-            self.sideup = 'Heads'
+            self.sideup = "Heads"  # attribute
         else:
-            self.sideup = 'Tails'
+            self.sideup = "Tails"
 
     # The get_sideup method returns the value
     # referenced by sideup.
 
-    def get_sideup(self):
-            return self.sideup
+    def get_sideup(self):  # method
+        return self.sideup  # attribute
